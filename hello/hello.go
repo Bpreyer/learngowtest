250b5badde4837a6477c2c8375439da9ae549eab
@bpreyer <b.a.preyerjr@gmail.com>
2021-05-20 10:26:33 -0400

    finished hello

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,15 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 // Hello is the function to print hello
 
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
