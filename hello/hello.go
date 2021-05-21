7d5e9a23d4844a53b567e2e14ea1c69bb167a267
@bpreyer <b.a.preyerjr@gmail.com>
2021-05-21 10:42:15 -0400

    finished hello for real this time

--- hello/hello.go
+++ hello/hello.go
@@ -12,11 +12,28 @@ func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	if language == spanish {
-		return spanishHelloPrefix + name
-	}
-	if language == french {
-		return frenchHelloPrefix + name
-	}
-	return englishHelloPrefix + name
+	return greetingPrefix(language) + name
 }
+
+// is a switch for the changing of the language prefix
+// lower case start to func keeps it private while upper is public
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+
+	}
+	return
+}
+
+// if language == spanish {
+// 	return spanishHelloPrefix + name
+// }
+// if language == french {
+// 	return frenchHelloPrefix + name
+// }
+// return englishHelloPrefix + name
